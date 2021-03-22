54e0b0a9e2230374f32e4c2149740e4b3407af2e
janaina-mj <janaina.marostega@hotmail.com>
2021-03-22 10:26:56 +0000

    feat: Add a dynamically updating navigational menu

--- js/app.js
+++ js/app.js
@@ -17,6 +17,11 @@
  * Define Global Variables
  * 
 */
+//store the ul element
+const navigationBar = document.getElementById('navbar__list');
+//store all content sections
+const sections = document.querySelectorAll('section[data-nav]');
+
 
 
 /**
@@ -34,6 +39,26 @@
 */
 
 // build the nav
+//create a function to build all <li> elements
+function buildNav() {
+    /*This function will build li elements using Virtual DOM to avoid reflow while the iterations happen
+    loop over all content sections to create li elements and give them inner content
+    append all <li> elements to the ul element
+    append the fragment to the ul element
+    */
+    const fragment = document.createDocumentFragment();
+    
+    for(let section of sections){
+        let navLink = document.createElement('li'); 
+        navLink.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        
+        fragment.appendChild(navLink);
+    }
+    
+    navigationBar.appendChild(fragment);
+};
+
+buildNav();
 
 
 // Add class 'active' to section when near top of viewport
